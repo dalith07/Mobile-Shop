6d45b476176ca800bfc6a40ed1d47cf735964825
kira <mohammedalitheiri@gmail.com>
2025-10-09 14:30:13 +0100

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,31 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "*.ufs.sh", // ✅ allow all subdomains
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "ufs.sh", // ✅ just in case the root domain is used
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "example.com", // add this
+        pathname: "/**",
+      },
+      // if your API returns from more domains, add them here
+    ],
+  },
 };
 
 export default nextConfig;
